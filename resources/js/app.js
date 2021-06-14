ef9c84192c813638b2a5b3708bda955a613b5dee
Job <LordChunk@users.noreply.github.com>
2021-06-14 17:52:11 +0200

    Setup Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,34 @@
 import Vue from "vue";
+
+// Components
 import Nav from "../components/Nav.vue"
+import Home from "../components/Home.vue"
+import Login from "../components/Login.vue"
+
+// Plugins
 import vuetify from './plugins/vuetify'
+import router from './plugins/router'
 
 require('./bootstrap.js')
 
 Vue.component('navigation', Nav);
+Vue.component('home', Home);
+Vue.component('login', Login);
+
+router.addRoutes([
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/login',
+        component: Login,
+    }
+]);
 
 const app = new Vue({
     vuetify,
+    router,
     el: '#vue-app',
     components: {
         Nav
