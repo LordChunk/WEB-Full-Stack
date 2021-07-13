3fde6aa0357476f64d7ce8903e0cdf87b3e62863
Job <LordChunk@users.noreply.github.com>
2021-07-13 14:11:56 +0200

    Added migrations

---
+++ database/migrations/2000_06_06_000000_import_old_database.php
@@ -0,0 +1,73 @@
+<?php
+
+use App\Models\Dish;
+use App\Models\DishDescription;
+use App\Models\DishType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class ImportOldDatabase extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Import old database
+        DB::unprepared( file_get_contents( __DIR__. "/initial.sql" ) );
+
+        // Drop unused tables
+        Schema::dropIfExists('specialiteiten');
+        Schema::dropIfExists('rijst_enzo');
+        Schema::dropIfExists('menu_pdf');
+
+        // Drop tables with unused data
+        Schema::dropIfExists('gebruiker');
+
+        // Shuffle names (this can't be done earlier as some table names are set in the actual .sql file)
+        Schema::rename('menu', 'old_menu');
+        Schema::rename('new_menu', 'menu');
+
+        // Migrate over old menu data
+        DB::table('old_menu')->eachById(function ($oldDish) {
+            // Add dish types to db
+            $dishType = DishType::firstOrCreate(['name' => $oldDish->soortgerecht]);
+
+            // Add descriptions
+            $dishDescription = null;
+            if (strlen($oldDish->beschrijving) > 0) {
+                $dishDescription = DishDescription::create(['description' => $oldDish->beschrijving]);
+            }
+
+            // Create actual dish entry
+            $newDish = new Dish;
+
+            $newDish->menu_indicator = $oldDish->menunummer . $oldDish->menu_toevoeging;
+            $newDish->name = $oldDish->naam;
+            $newDish->price = $oldDish->price;
+
+            // Link foreign elements to dish
+            $newDish->type()->associate($dishType);
+            $newDish->description()->associate($dishDescription);
+
+            $newDish->save();
+        });
+
+        // Drop old menu data
+        Schema::dropIfExists('old_menu');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/seeders/NavigationItemSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\NavigationItem;
+use Illuminate\Database\Seeder;
+
+class NavigationItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        NavigationItem::create([
+            'name'=>'Home',
+            'url'=>'/',
+        ]);
+        NavigationItem::create([
+            'name'=>'Menu',
+            'url'=>'/menu',
+        ]);
+        NavigationItem::create([
+            'name'=>'Login',
+            'url'=>'/login',
+        ]);
+        NavigationItem::create([
+            'name'=>'Hello World',
+            'url'=>'/hello-world',
+        ]);
+    }
+}
