52e777feb392bb528339b8828ebaa46c69b8536c
Job <LordChunk@users.noreply.github.com>
2021-07-13 14:45:40 +0200

    Changed some configs

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue'
 
 createInertiaApp({
-  resolve: name => require(`../pages/${name}`),
+  resolve: name => require(`./Pages/${name}`),
   setup({ el, app, props }) {
     new Vue({
       render: h => h(app, props),
