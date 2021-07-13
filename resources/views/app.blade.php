27bb640707c9e90687d6f44e8e619dbebb7a7c54
Job <LordChunk@users.noreply.github.com>
2021-07-13 14:41:13 +0200

    Setup Inertia Vue integration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Web Full Stack</title>
+
+        <!-- Fonts -->
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+
+        <!-- JavaScript -->
+        <script src="{{ mix('/js/app.js') }}" defer></script>
     </head>
     <body>
         @inertia
