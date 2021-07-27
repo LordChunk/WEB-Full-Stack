cb83b0fa11a61cb52c8a0ad6634f5df7369d1237
Job <LordChunk@users.noreply.github.com>
2021-07-27 14:30:05 +0200

    Added funky basket

--- resources/js/Pages/Employee/Menu.vue
+++ resources/js/Pages/Employee/Menu.vue
@@ -14,14 +14,20 @@
       >
       </dish-type>
     </div>
-    <div class="basket">
-      <div class="basket-header">
+    <div
+      class="cart container px-4"
+      v-bind:class="{ 'open-cart': isCartOpen, 'hide-cart': $store.state.cart.length === 0 }"
+    >
+      <div class="draggable" @click="clickCart">
+        <img src="/images/drag_handle_black_24dp.svg" />
+      </div>
+      <div class="cart-header">
         <h2>Winkelmand</h2>
       </div>
       <div>
         <div v-for="dish in $store.state.cart" :key="dish.id">
           <p>
-            {{dish.name}}
+            {{ dish.name }}
           </p>
         </div>
       </div>
@@ -29,11 +35,66 @@
   </div>
 </template>
 
+<style scoped lang="scss">
+.menu-header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.cart {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  border-radius: 1rem 1rem 0 0;
+  background: #bdf4ff;
+  box-shadow: 0px 9px 15px 15px rgb(0 0 0 / 17%);
+
+  // Creates proper CSS height animation
+  // https://stackoverflow.com/questions/3508605/how-can-i-transition-height-0-to-height-auto-using-css
+  height: 100vh;
+  max-height: 2em;
+
+  .draggable {
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    img {
+      width: 2em;
+    }
+  }
+  transition: max-height 300ms;
+}
+.open-cart {
+  max-height: 100vh;
+}
+.hide-cart {
+  max-height: 0;
+}
+// Style all dish elements according to employee layout
+::v-deep .dish-wrapper {
+  padding: 1.5em 0;
+
+  .dish-price {
+    font-size: 1.2em;
+    font-weight: bold;
+  }
+
+  // Remove margin from last p tag
+  p:last-child {
+    margin: 0;
+  }
+}
+</style>
+
 <script>
 import Layout from "@/Shared/BootstrapLayout.vue";
 import DishType from "@/Components/DishCategory.vue";
 export default {
   layout: Layout,
+  components: {
+    DishType,
+  },
   props: {
     dishTypes: {
       type: Array[
@@ -51,43 +112,23 @@ export default {
       required: true,
     },
   },
-  components: {
-    DishType,
+  data() {
+    return {
+      isCartOpen: false,
+    };
   },
   beforeCreate() {
-    if(this.$store.cart === undefined) {
-      this.$store.cart = [];
+    if (this.$store.state.cart === undefined) {
+      this.$store.state.cart = [];
     }
   },
   methods: {
     emptyCart() {
-      this.$store.clear("cart");
+      this.$store.state.cart = [];
+    },
+    clickCart() {
+      this.isCartOpen = !this.isCartOpen;
     },
   },
 };
 </script>
-
-<style scoped lang="scss">
-.menu-header {
-  display: flex;
-  justify-content: space-between;
-}
-
-// .basket {
-//   display: absolute;
-// }
-// Style all dish elements according to employee layout
-::v-deep .dish-wrapper {
-  padding: 1.5em 0;
-
-  .dish-price {
-    font-size: 1.2em;
-    font-weight: bold;
-  }
-
-  // Remove margin from last p tag
-  p:last-child {
-    margin: 0;
-  }
-}
-</style>
