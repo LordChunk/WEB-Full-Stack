779715260b916553a66370b382a890e9142f0045
Job <LordChunk@users.noreply.github.com>
2021-08-10 14:38:14 +0200

    Fixed issues with corrupted depency files and setup i18n initialisation

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 
-import VueI18n from 'vue-i18n';
+import { createI18n } from 'vue-i18n';
 
 // Plugins
 import { ZiggyVue } from 'ziggy';
@@ -16,12 +16,18 @@ import { InertiaLink } from '@inertiajs/inertia-vue3'
 
 InertiaProgress.init()
 
+const i18n = createI18n({
+  locale: 'nl', // set locale
+  fallbackLocale: 'en', // set fallback locale
+  messages: {},
+})
 createInertiaApp({
     resolve: name => require(`./Pages/${name}`),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(i18n)
             .use(StatePlugin)
             .use(OrderPlugin)
             .component('inertia-link', InertiaLink)
